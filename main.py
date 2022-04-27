0bb4d1fddcd28f27716140cfca69eae47a4da209
Teodor Moscaliuc <79260114+RedKyet@users.noreply.github.com>
2022-04-27 08:59:59 +0000

    updated scoring system

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ distoken = os.environ['token']
 client=discord.Client()
 join_message_sent = False
 join_list={}
+eggs_broken={}
 game_start=False
 b_phase=1
 
@@ -22,6 +23,7 @@ async def on_message(message):
   global game_start
   global who_said_hai
   global b_phase
+  global eggs_broken
   
   if message.author == client.user:
     return
@@ -39,19 +41,21 @@ async def on_message(message):
       while len(join_list)>1: await asyncio.sleep(5)
       game_start=False
       for x in join_list: winner=x.name
-      await message.channel.send('**Game has ended. {} won**'.format(' '.join(x.mention for x in join_list)))
+      for x in join_list: winner_tag=x
+      await message.channel.send('**Game has ended. {} won {} pts**'.format(' '.join(x.mention for x in join_list), eggs_broken[winner_tag]))
       with open("leaderboard.json", "r+") as lb:
         data = json.load(lb)
         if winner not in data:
-          data.update({winner:1})
+          data.update({winner:eggs_broken[winner_tag]})
         else:
-          data[winner]+=1
+          data[winner]+=eggs_broken[winner_tag]
         data = dict(sorted(data.items(), key=lambda x:x[1], reverse=True))
         # print(sortdict)
         lb.seek(0)
         json.dump(data,lb)
       join_message_sent=False
       join_list={}
+      eggs_broken={}
     else: 
       await message.channel.send('Nobody joined :(')
       join_message_sent = False
@@ -59,6 +63,7 @@ async def on_message(message):
     
   elif message.content == '🥚' and join_message_sent==True and message.author not in join_list:   
     join_list[message.author] = random.randint(1,5)
+    eggs_broken[message.author] = 0
     who_said_hai=message.author
 
   elif message.content == "🥚 Hristos a inviat" or message.content == "🥚Hristos a inviat" or message.content == "🥚 Hristos a înviat" or message.content == "🥚Hristos a înviat" and game_start==True and b_phase==1 and message.author in join_list:
@@ -70,13 +75,20 @@ async def on_message(message):
     # print(message.content)
     if join_list[message.author] > join_list[who_said_hai]:
       await message.channel.send("{}'s egg brakes :(".format(who_said_hai.mention))
+      eggs_broken[message.author]+=1
       join_list.pop(who_said_hai)
     elif join_list[message.author] < join_list[who_said_hai]:
       await message.channel.send("{}'s egg brakes :(".format(message.author.mention))
+      eggs_broken[who_said_hai]+=1
       join_list.pop(message.author)
     else:
+      tmp=who_said_hai
       who_said_hai=random.choice([who_said_hai, message.author])
       await message.channel.send("{}'s egg brakes :(".format(who_said_hai.mention))
+      if who_said_hai==message.author:
+        eggs_broken[tmp]+=1
+      else:
+        eggs_broken[message.author]+=1
       join_list.pop(who_said_hai)
     b_phase=1
   elif message.content == "&help":
